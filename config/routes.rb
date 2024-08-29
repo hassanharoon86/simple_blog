9d534f583d22266f24f9c16c807140c1dddee1d8
Hassan Haroon <hassanharoon86@gmail.com>
2024-08-30 00:53:25 +0500

    Likes CRUD: Model and DB Table

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :posts
+  resources :posts do
+    resource :like, only: [:create, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
